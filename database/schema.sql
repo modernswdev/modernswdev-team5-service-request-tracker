55098033bb921a2884c803d3fbfa9e7174af0365
Matthew-Ingram <99311642+Matthew-Ingram@users.noreply.github.com>
2026-03-30 03:51:23 +0000

    Pytest demanded there to be no errors at all, so I updated schema.sql with Users table.

--- database/schema.sql
+++ database/schema.sql
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS requests (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS Users
+(
+	UserID		INTEGER		NOT NULL,
+	UserFirstName	TEXT		,
+	UserLastName	TEXT		,
+	UserEmail	TEXT		NOT NULL UNIQUE,
+	UserPassword	TEXT		NOT NULL,
+	UserRole	INTEGER		NOT NULL CHECK(UserRole >= 0 AND UserRole <= 3) DEFAULT 0, -- 0 = No Login, 1 = User, 2 = Staff, 3 = Admin --
+	UserCreateDate	TEXT		DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT	Users_PK	PRIMARY KEY(UserID)
+	-- Spiritual Foreign Key - The foreign keys don't work the way I wanted them to, so refer to this for triggers. --
+	-- CONSTRAINT	Assignees_FK	FOREIGN KEY(UserID) REFERENCES Assignees(AssigneeHandlerID) ON DELETE CASCADE
+);
